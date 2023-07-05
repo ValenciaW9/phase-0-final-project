6f51eb212557c9957e9f9d49a2a338385023c220
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-07-05 11:42:40 -0400

    Phase 0 Final Project
    Phase 0 Final Project

---
+++ phase-0-final-project-main/index.js
@@ -0,0 +1,36 @@
+<!DOCTTOW hrml>
+<html long= "en">
+   
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv= "X-UA-Compatible" content= "IE"=edge">
+         <meta name ="viewport" content ="width=device-width, intial-scale=1.0">
+        <title>Compalete Responsive Personal Portfolio Website HtML CSS Javascriot | C
+     adehal</title>
+       
+        <link rel - "stylesheet" href="c ss/style.css">
+            
+        <! - - box ixons - ->
+        <link href= 'https:// unpkg.come/boxixdons@@.2.1.4/css/boxicons@2.1.4/css/boxicons.min.css" rel= 'style sheet'>
+        <link href= 'hrttps://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='style sheet'>
+            </head>
+
+            <body>
+        <! -- header design - ->
+
+        <! - - home section design - ->
+
+        <! - - about section design - ->
+ Hello, my name is Valencia Walker, Im an aspiring Software Engineer attending Flat Iron School. I'm also completing My Masters of Science in Psychology at The University of Phoenix online. I'm a Christian. I enjoy spending time with loved ones. My interests are in Music, Art, Fashion, and video games. I enjoy reading and researching.
+
+        <!- - education section design - ->
+ My Background academic education I have a B.S. in Health Administration From the Univeristy of Michigan-FLint I obtained in 2018. And an A.S. in Health Administration From Colorado Technical University in 2013. Also, I'm currently pursuing an M.S. in Psychology from the University of Phoenix. Im also currently pursuing Software Engineering At Flat Iron School.  
+        <! - - skills section design - ->
+        My skills are Mircosoft Word Suite, Physiology skills, Beginner Python, Beginner Javascript, HTML, and CSS beginner SQL in addition to learning other languages like typescript, react, Fkask, Algorithms, Databases, and Data Structures. TDD and OOP, Recursion, Python, SQL, Ruby, Javascript, Python, HTML & CSS, Object Oriented Programing-Python&Javascript, Data Structures and Algorithms, Sorting,  Trees and Binary Search, Graphs, Whiteboarding, Servers, APIs, and Promises, Asynchronous Javascript, APIs, Networks, Frontend Javascript, Working with the DOM, Express &SQL, Intermediate Express-Middleware, Routers & Environmental Variables, SQL, Express & Sequelize, Real World Sequalize APplications, Web Security and Authentication, Backend Authentication, Redux and Authentication, Redux Thunk, Python Data types, Functions and Statements, Structured Data, Python Environment & Testing, Flasl, SQLAlchemy, and Forms. Docker and Microservices and Microservice applied.
+
+        <! - - contact section design - ->
+        My contact information: Github:https://github.com/ValenciaW9 ValenciaWalker37@yahoo.com ,Valenciw90@email.phoenix.edu  https://www.linkedin.com/in/valencia-walker-306a7477/  , Phone number:( 810) 348-2944.
+
+        < - - footer design - ->
+
+
